3a24d5090d417d2ad611ef70ab695792b89b8d37
lucienwoo <lucienwoo@163.com>
2014-05-10 23:36:23 +0800

    添加配置文件，修正温度错位问题

--- showweather.py
+++ showweather.py
@@ -13,4 +13,9 @@ class WeatherShow:
 
 
     def __combine_info(self):
-        return "天气:"+self.weather.weather+"\n"+"当前温度:"+self.weather.temp+"\n"+"最低温度:"+self.weather.temp1+"    "+"最高温度:"+self.weather.temp2+"\n"+"风向:"+self.weather.wd+"\n"+"风速:"+self.weather.ws+"\n"+"湿度:"+self.weather.sd+"\n"+"发布时间:"+self.weather.time
+        temp1_str="最低温度"
+        temp2_str="最高温度"
+        if self.weather.temp1>self.weather.temp2:
+            temp1_str,temp2_str=temp2_str,temp1_str
+
+        return "天气:"+self.weather.weather+"\n"+"当前温度:"+self.weather.temp+"\n"+temp1_str+self.weather.temp1+"    "+temp2_str+self.weather.temp2+"\n"+"风向:"+self.weather.wd+"\n"+"风速:"+self.weather.ws+"\n"+"湿度:"+self.weather.sd+"\n"+"发布时间:"+self.weather.time
